6e06be7a93ea256daf2ece608c48d1e4776a853c
Alekseev Michael <misha.alex94@gmail.com>
2020-10-23 04:25:04 +0500

    Initial commit

---
+++ AndroidApp/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Net.Http;
+using System.Threading;
+
+namespace AndroidApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstace)
+        {
+            base.OnCreate(savedInstace);
+            var httpclient = new HttpClient();
+            CancellationTokenSource tokenSource = new CancellationTokenSource(2_000); //Ответ на вопрос 3: Создаем переменную вне контекста инструкции using.
+            using (tokenSource)  
+            {
+                try
+                {
+                    httpclient.GetAsync("https://google.com:4552", tokenSource.Token).GetAwaiter().GetResult(); //Отсутствие ответа от сервера не является ошибкой задания
+                }
+                catch
+                {
+                }
+                finally
+                {
+                }
+            }
+            SetContentView(Resource.Layout.activity_main); /*Ответы на вопросы 1. и 2.:
+                                                            * инструкция using создает контекст в котором существует переменная tokenSource, в указанной точке кода в соответствии с принципами ООП 
+                                                            * переменная tokenSource перестает существовать и не может быть в дальнейшем использована. В случае попытки обращения к ней
+                                                            * среда разработки укажет на ошибку, что tokenSource не существует в данном контексте */
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpclient = new HttpClient();
+            using CancellationTokenSource tokenSource = new CancellationTokenSource(2_000);
+
+            /*using CancellationTokenSource tokenSource = new CancellationTokenSource(2_000)
+             *является важным элементом, без которого задача не может быть выполнена, именно токен отмены задает некий аналог if-else, который работает в блоке try-catch-finally
+             *Ну и, соответственно, использование конструктора GetAsync(String, CancellationToken) также является необходимым. Отсутствие токена в методе GetAsync может привести
+             *к бесконечно долгому ожиданию ответа от сервера без исключений, следовательно код не перейдет в блок catch и не сможет ничего обработать.*/
+
+            try
+            {
+                httpclient.GetAsync("https://google.com:4552", tokenSource.Token).GetAwaiter().GetResult(); //Отсутствие ответа от сервера не является ошибкой задания
+            }
+            catch
+            {
+            }
+            finally
+            {
+            }
+        }
+    }
+}
